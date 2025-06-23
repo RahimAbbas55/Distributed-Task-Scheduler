4c7941f3bb5a404d504181e3ac94e2fe2d26455a
RahimAbbas55 <rahim.abbas27@gmail.com>
2025-06-24 01:49:17 +0500

    Setup server and basic file structure for both front-end and back-end

---
+++ Backend/controller/jobController.js
@@ -0,0 +1,18 @@
+const supase = require('../lib/supabase');
+const { v4 : uuidv4 } = require('uuid');
+
+// Create job
+async function createJob(req , res){
+
+}
+// Get job by ID
+async function getJobByID(req , res){
+    
+}
+// Delete job by ID
+async function deleteJobByID(req , res){
+    
+}
+module.exports = {
+    createJob , getJobByID , deleteJobByID
+}
