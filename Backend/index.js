add277e9f76223ed98b7742dc73473b2933e160f
RahimAbbas55 <rahim.abbas27@gmail.com>
2025-07-03 14:36:30 +0500

    Allowed CORS

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,19 @@
 // Imports
 const express = require('express');
 const jobRoutes = require('./routes/jobRoutes');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Allow requests from front-end
+app.use(cors({
+  origin: "http://localhost:3000", // frontend origin
+  credentials: true
+}));
+
 // Configurations
 require('dotenv').config();
+app.use(cors());
 app.use(express.json());
 
 // Using the routes
